bfb4c1e2dff5ed5061e080753649db04dea7c17d
Eduardo Ferrer <smingolera@gmail.com>
2026-03-25 19:31:36 -0300

    chore: remove old stuff from repo

--- dctToSRBC.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")" || exit 1
-source .venv/bin/activate
-pip install -r requirements.txt
-python main.py
-read -p "Pressione Enter para continuar..."
--- run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Ativando ambiente virtual...
-call .venv\Scripts\activate
-
-echo Executando aplicacao...
-python main.py
-
-pause
